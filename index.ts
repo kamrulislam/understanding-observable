df75748525a0dc5debaaa2f05b64418308c8fa42
Md Kamrul Islam <kamruli@gmail.com>
2018-07-27 14:56:49 +1000

    Safe Observable

--- index.ts
+++ index.ts
@@ -5,15 +5,36 @@ export class DataSource {
     ondata: any;
     onerror: any;
     oncomplete: any;
-    destroy: any;
+    destroy() {};
+}
+
+export class SafeObserver {
+    unsub: any;
+    unsubscribe() {}
+    next(e) {};
+    error(err) {
+        this.unsubscribe();
+    };
+    complete() {
+        this.unsubscribe();
+    };
+
+    constructor(private observer: any){
+        this.observer.next = this.observer.next || this.next;
+        this.observer.error = this.observer.error || this.error;
+        this.observer.complete = this.observer.complete || this.next;
+    }
 }
 
 const myObservable = (observer) => {
+    const safeObserver = new SafeObserver(observer);
     const datasource = new DataSource();
-    datasource.ondata = (e) => observer.next(e);
-    datasource.onerror = (err) => observer.error(err);
-    datasource.oncomplete = () => observer.complete();
-    return () => {
+    datasource.ondata = (e) => safeObserver.next(e);
+    datasource.onerror = (err) => safeObserver.error(err);
+    datasource.oncomplete = () => safeObserver.complete();
+    safeObserver.unsub = () => {
         datasource.destroy();
     };
+
+    return safeObserver.unsubscribe.bind(safeObserver);
 }
