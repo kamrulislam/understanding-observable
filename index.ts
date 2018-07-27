aa02212d3a1a98d348b1e4ba4f5f70744dfd1e9a
Md Kamrul Islam <kamruli@gmail.com>
2018-07-27 14:40:28 +1000

    initial observable

--- index.ts
+++ index.ts
@@ -1,2 +1,19 @@
 // https://medium.com/@benlesh/learning-observable-by-building-observable-d5da57405d87
 console.log('test');
+
+export class DataSource {
+    ondata: any;
+    onerror: any;
+    oncomplete: any;
+    destroy: any;
+}
+
+const myObservable = (observer) => {
+    const datasource = new DataSource();
+    datasource.ondata = (e) => observer.next(e);
+    datasource.onerror = (err) => observer.error(err);
+    datasource.oncomplete = () => observer.complete();
+    return () => {
+        datasource.destroy();
+    };
+}
